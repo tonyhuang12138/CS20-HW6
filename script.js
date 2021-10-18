f97f108d5ae05f904720d1daf22320ab2c24fa80
Tony Huang <42039768+tonyhuang12138@users.noreply.github.com>
2021-10-17 21:30:12 -0400

    function support

--- script.js
+++ script.js
@@ -2,27 +2,31 @@ const form = document.getElementById("form")
 const num1 = document.getElementById("num1")
 const num2 = document.getElementById("num2")
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault(); // Prevent redireciton
+function amicable() {
     // Prepare the two numbers for comparison
-    var num1Val, num2Val, num1FactorsSum = 0, num2FactorsSum = 0
+    var num1Val, num2Val, num1Factors = [], num2Factors = [], n1fDisplay = "", n2fDisplay = "", num1fSum = 0, num2fSum = 0
     num1Val = num1.value
     num2Val = num2.value
     // Find the factors of the two numbers
     for (let i = 0; i < num1Val; i++) {
         if (num1Val % i == 0) {
-            num1FactorsSum += i 
+            num1Factors.push(i)
+            n1fDisplay += " " + i
+            num1fSum += i
         }
     }
     for (let i = 0; i < num2Val; i++) {
         if (num2Val % i == 0) {
-            num2FactorsSum += i 
+            num2Factors.push(i)
+            n2fDisplay += " " + i
+            num2fSum += i
         }
     }
+    alert("Factors of number 1 are:" + n1fDisplay + '\n' + "Factors of number 2 are:" + n2fDisplay)
     // Check if they are amicable
-    if (num1FactorsSum == num2Val && num2FactorsSum == num1Val) {
-        document.getElementById("result").innerHTML = "The numbers: " + num1Val + " and " + num2Val + " are amicable";
+    if (num1fSum == num2Val && num2fSum == num1Val) {
+        document.getElementById("result").textContent = "The numbers: " + num1Val + " and " + num2Val + " are amicable"
     } else {
-        document.getElementById("result").innerHTML = "The numbers: " + num1Val + " and " + num2Val + " are not amicable";
+        document.getElementById("result").textContent = "The numbers: " + num1Val + " and " + num2Val + " are not amicable"
     }
-})
+}
